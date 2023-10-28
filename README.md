bc7169bcb7d2d0580696dd479a61ac9e78ccb094
ZD0341 <144489565+ZD0341@users.noreply.github.com>
2023-10-28 15:07:10 -0700

    more js

--- README.md
+++ README.md
@@ -1,5 +1,37 @@
 # Web-API-challenge
+  
+  ## Description
+  This is an e-commerce application developed using Node.js, Express, and Sequelize. The application allows users to manage products, categories, and tags, enabling an end-to-end e-commerce experience.
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  1. Clone this repository.
+  2. Install dependencies using `npm install`.
+  3. Configure your database connection by creating a `.env` file with your environment variables, like `DB_NAME`, `DB_USER`, `DB_PW`, and `DB_HOST`.
+  4. Run the database schema and seed data using `npm run seed`.
+  5. Start the server with `npm start`
+  
+  ## Usage
+  - Access the API routes for managing categories, products, and tags.
+  - Use tools like Postman or cURL to make API requests.
+  - Refer to the API documentation for detailed endpoints.
+  
+  ## License
+  This project is licensed under the License1 License.
+  
+  ## Contributing
+  Feel free to contribute by opening issues or submitting pull requests to enhance the application.
 
-app does not function.
-incomplete, in the process of debugging but ran out of time.
-will resubmit
+  
+  ## Tests
+  no test code has been written
+  
+  ## Questions
+  If you have any questions, you can reach out to [ZD0341](https://github.com/ZD0341) or contact via email at zechiel97@yahoo.com.
--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,14 @@ var startBtn = document.querySelector("#start_button");
 var introPage =document.querySelector("#intro_page");
 
 var questionPage = document.querySelector("#question_page");
+var askQuestion = document.querySelector("#ask_question");
+
+var reactButtons = document.querySelectorAll(".choices");
+var answerBtn1 = document.querySelector("#answer_btn1");
+var answerBtn2 = document.querySelector("#answer_btn2");
+var answerBtn3 = document.querySelector("#answer_btn3");
+var answerBtn4 = document.querySelector("#answer_btn4");
+
 
 var questionSource = [
     {
@@ -40,66 +48,123 @@ var questionSource = [
     },
 ];
 
+var timeLeft = document.getElementById("timer");
+
+var secondsLeft = 60;
 var questionNumber = 0;
-
-
+var totalScore = 0;
+var questionCount = 1;
 /*Functions*/
 //WHEN I click the start button, THEN a timer starts(The setInterval() Method)
-function countdown() { 
+function countdown() {
+        
     var timerInterval = setInterval(function () {
 
-        secondsLeft--;
-        timeLeft.textContent = "Time left: " + secondsLeft + " s";
-  
-          if (secondsLeft <= 0){
-              clearInterval(timerInterval);
-              timeLeft.textContent = "Time is up!"; 
-              finish.textContent = "Time is up!";
-              gameOver();
+      secondsLeft--;
+      timeLeft.textContent = "Time left: " + secondsLeft + " s";
 
-          } else  if(questionCount >= questionSource.length +1) {
-              clearInterval(timerInterval);
-              gameOver();
-              } 
-  }, 1000);
+        if (secondsLeft <= 0){
+            clearInterval(timerInterval);
+            timeLeft.textContent = "Time is up!"; 
+            // if time is up, show on score board content instead of "all done!"
+            finish.textContent = "Time is up!";
+            gameOver();
+
+        } else  if(questionCount >= questionSource.length +1) {
+            clearInterval(timerInterval);
+            gameOver();
+            } 
+}, 1000);
 }
 
 //Click the button to start the quiz
-function startQuiz() {
+function startQuiz () {
     introPage.style.display = "none";
     questionPage.style.display = "block";
     questionNumber = 0
     countdown();
-        
     showQuestion(questionNumber);
+  
 }
-//present the questions and answers
-function showQuestion() {
-    const question = questionSource[currentQuestion];
-    questionTitle.textContent = question.question;
-    choicesContainer.innerHTML = "";
-
-    question.choices.forEach((choice, index) => {
-        const choiceButton = document.createElement("button");
-        choiceButton.textContent = choice;
-        choiceButton.setAttribute("data-index", index);
-        choiceButton.addEventListener("click", checkAnswer);
-        choicesContainer.appendChild(choiceButton);
-    });
+//present the questions and answers to user
+function showQuestion (n) {
+    askQuestion.textContent = questionSource[n].question;
+    answerBtn1.textContent = questionSource[n].choices[0];
+    answerBtn2.textContent = questionSource[n].choices[1];
+    answerBtn3.textContent = questionSource[n].choices[2];
+    answerBtn4.textContent = questionSource[n].choices[3];
+    questionNumber = n;
 }
 
-//WHEN I answer a question,Show if answer is correct or wrong 
-function checkAnswer() { }
-//WHEN all questions are answered or the timer reaches 0, Game is over
-function gameOver() { };
+function checkAnswer(event) {
+event.preventDefault();
+//make it display
+checkLine.style.display = "block";
+setTimeout(function () {
+    checkLine.style.display = 'none';
+}, 1000);
 
-// get current score and initials from local storage
-function getScore() { };
+// answer check
+if (questionSource[questionNumber].answer == event.target.value) {
+    checkLine.textContent = "Correct!"; 
+    totalScore = totalScore + 1;
+
+} else {
+    secondsLeft = secondsLeft - 10;
+    checkLine.textContent = "Wrong! The correct answer is " + questionSource[questionNumber].answer + " .";
+}
+     //THEN I am presented with another question
+if (questionNumber < questionSource.length -1 ) {
+// call showQuestions to bring in next question when any reactBtn is clicked
+    showQuestion(questionNumber +1);
+} else {
+gameOver();
+}
+questionCount++;
+}
+function gameOver() {
+
+    questionPage.style.display = "none";
+    scoreBoard.style.display = "block";
+    console.log(scoreBoard);
+    finalScore.textContent = "Your final score is :" + totalScore ;
+    timeLeft.style.display = "none"; 
+};
+
+function getScore () {
+var currentList =localStorage.getItem("ScoreList");
+if (currentList !== null ){
+    freshList = JSON.parse(currentList);
+    return freshList;
+} else {
+    freshList = [];
+}
+return freshList;
+};
 
 
-// render score to the score board
-function renderScore() { };
+function renderScore () {
+scoreRecord.innerHTML = "";
+scoreRecord.style.display ="block";
+var highScores = sort();    
+var topFive = highScores.slice(0,5);
+for (var i = 0; i < topFive.length; i++) {
+    var item = topFive[i];
+var li = document.createElement("li");
+li.textContent = item.user + " - " + item.score;
+li.setAttribute("data-index", i);
+scoreRecord.appendChild(li);
+}
+};
 
-// sort score and ranking the highscore list
-function sort() { };
+function sort () {
+var unsortedList = getScore();
+if (getScore == null ){
+    return;
+} else{
+unsortedList.sort(function(a,b){
+    return b.score - a.score;
+})
+return unsortedList;
+}};
 
